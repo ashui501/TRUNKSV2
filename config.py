a38720cecff58c3b087c53694cd121fd9fbde54d
Ashui <113001130+ashui501@users.noreply.github.com>
2024-05-06 13:55:23 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+OWNER_ID = os.getenv("OWNER_ID",1238234357)
+HANDLER = [".","!","@"]
+LOVELY = "https://te.legra.ph/file/794a9a75ae32566d41853.mp4"
+GROUP_ID = os.getenv("GROUP_ID", "-1002119409366")
+SOURCE = "https://github.com/Darklightning2008/LovelyX"
+LOG_CHAT = -1002119409366
